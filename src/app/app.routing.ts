e8b02fe0911813be0e9cb74ead5679df4fb1d068
Daniel Laera <laera.daniel@hotmail.com>
2018-09-09 17:27:53 +0200

    lazy loading

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ShoppingListComponent } from './features/shopping-list/shopping-list.component';
+import { HomeComponent } from './features/home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
+  { path: 'recipes', loadChildren: './features/recipes/recipes.module#RecipesModule'},
   { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
